72e44a360a68ce8e5339a9db92ff3ecb3d0cc764
nevilkrishna <nevilkrishna@gmail.com>
2022-12-25 11:14:56 +0530

    created  calculator

--- calculator.js
+++ calculator.js
@@ -1,9 +1,18 @@
 const express = require("express");
-
+const bodyParser = require("body-parser");
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get("/", (req, res) => {
-  res.sendFile(__dirname+"/index.html");
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", (req, res) => {
+  var x = Number(req.body.num1)
+  var y = Number(req.body.num2)
+  var sum = x+y;
+  res.send(`SUM: ${sum}`);
 });
 
 app.listen(3000, () => {

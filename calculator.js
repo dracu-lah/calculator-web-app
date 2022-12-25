20194ab20858079899ed42594a4935b8620ab652
nevilkrishna <nevilkrishna@gmail.com>
2022-12-25 11:16:31 +0530

    fixed some bugs

--- calculator.js
+++ calculator.js
@@ -2,16 +2,16 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true })); // to use body parser
 
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
+  res.sendFile(__dirname + "/index.html"); // __dirname prints the pwd
 });
 
 app.post("/", (req, res) => {
-  var x = Number(req.body.num1)
-  var y = Number(req.body.num2)
-  var sum = x+y;
+  var x = Number(req.body.num1);
+  var y = Number(req.body.num2);
+  var sum = x + y;
   res.send(`SUM: ${sum}`);
 });
 
